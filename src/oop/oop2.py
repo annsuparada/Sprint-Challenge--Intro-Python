1a34686e19326b43ababa3998f3aedaee04a0438
Suparada Saitalae <ann.suparada@gmail.com>
2019-12-06 11:22:24 -0600

    oop2

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,12 +4,12 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
-
-    # TODO
-
-
+    def drive(self):
+        return "vroooom"
+    def __str__(self):
+        return f"{self.num_wheels}"
 # Subclass Motorcycle from GroundVehicle.
 #
 # Make it so when you instantiate a Motorcycle, it automatically sets the number
@@ -19,14 +19,26 @@ class GroundVehicle():
 
 # TODO
 
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=4):
+        self.num_wheels = num_wheels
+        GroundVehicle.__init__(self, num_wheels = 2)
+    def drive(self):
+        return "BRAAAP!!"
+    def __str__(self):
+        return f"{self.num_wheels}"
 vehicles = [
-    GroundVehicle(),
-    GroundVehicle(),
-    Motorcycle(),
-    GroundVehicle(),
-    Motorcycle(),
+    GroundVehicle(0),
+    GroundVehicle(0),
+    Motorcycle(0),
+    GroundVehicle(0),
+    Motorcycle(0),
 ]
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+
+for x in vehicles:
+    x.drive()
+    print(x)
